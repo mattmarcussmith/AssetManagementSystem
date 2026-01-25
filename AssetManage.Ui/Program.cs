d23c33e9dbe53b1c81af4b1a155f2f9f67040024
Matthew Smith <matt.marcus.smith@gmail.com>
2026-01-25 13:33:40 -0500

    Project structure fixed

--- AssetManage.Ui/Program.cs
+++ AssetManage.Ui/Program.cs
@@ -1,6 +1,6 @@
+using AssetManage.Ui;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using OrderProcessing.Ui;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
